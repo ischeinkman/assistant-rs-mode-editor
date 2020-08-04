7c9654ac014d453c54977118d89817a1693e18e3
ischeinkman <scheinkman.ilan@gmail.com>
2020-08-04 13:30:17 -0700

    Added some useful requirements and scripts.

--- mode_editor/webpack.config.js
+++ mode_editor/webpack.config.js
@@ -1,12 +1,18 @@
 const path = require('path');
 
 module.exports = {
-    mode : "development",
-    entry : "./src/index.js", 
-    output : {
-        filename : "main.js", 
-        path : path.resolve(__dirname, "dist"),
-        
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+
     },
-    devtool : 'inline-source-map',
-}
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.handlebars$/, loader: "handlebars-loader" }
+        ],
+    },
+};
