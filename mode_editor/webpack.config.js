4482869361c9b559072a44b45c21876fa36cce17
ischeinkman <scheinkman.ilan@gmail.com>
2020-08-04 16:36:13 -0700

    Minimize bundles.

--- mode_editor/webpack.config.js
+++ mode_editor/webpack.config.js
@@ -11,8 +11,12 @@ module.exports = {
     devtool: 'inline-source-map',
     module: {
         rules: [
-            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'], sideEffects: true, },
             { test: /\.handlebars$/, loader: "handlebars-loader" }
         ],
+    },
+    optimization: {
+        usedExports: true,
+        minimize : false, 
     },
 };
