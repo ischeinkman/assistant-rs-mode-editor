81ce3476ec4b6d6bafc936685d84db3710bafc3a
ischeinkman <scheinkman.ilan@gmail.com>
2020-08-11 17:10:25 -0700

    Now start on root modelist with upload button.

--- mode_editor/src/index.js
+++ mode_editor/src/index.js
@@ -1,16 +1,38 @@
 import './index.css';
 
-async function cb(evt) {
-    const modeldata_promise = import(/* webpackChunkName: `modeldata-index-1` */ './modeldata');
-    let newdata_promise = import(/* webpackChunkName: `dataloading-index-1` */'./loading')
-        .then(module => module.parse_files(evt.target.files));
-    const [modeldata, data] = await Promise.all([modeldata_promise, newdata_promise]);
-    await modeldata.loadData(data);
-    const mode_list = await import(/* webpackChunkName: `modellist-index-1` */'./components/modeListView/mode_list');
+
+/**
+ * @type {import('./components/modeListView/mode_list').ModeListView}
+ */
+var rootView;
+
+async function uploadFile() {
+    let elm = document.createElement('input');
+    elm.setAttribute('type', 'file');
+    elm.setAttribute('multiple', 'true');
+    elm.onchange = async (evt) => {
+        const modeldata_promise = import(/* webpackChunkName: `modeldata-index-1` */ './modeldata');
+        let newdata_promise = import(/* webpackChunkName: `dataloading-index-1` */'./loading')
+            .then(module => module.parse_files(evt.target.files));
+        const [modeldata, data] = await Promise.all([modeldata_promise, newdata_promise]);
+        await modeldata.loadData(data);
+        const modenames = await modeldata.getModeNames();
+        rootView.modenames = modenames;
+        rootView.reloadView();
+    };
+    elm.click();
+}
+
+document.addEventListener("DOMContentLoaded", mymain);
+
+async function mymain() {
+    const modeldata = await import(/* webpackChunkName: `modeldata-index-1` */ './modeldata');
     const modenames = await modeldata.getModeNames();
-    let mode_view = mode_list.makeList(modenames, {
+    const mode_list = await import(/* webpackChunkName: `modellist-index-1` */'./components/modeListView/mode_list');
+    rootView = mode_list.makeList(modenames, {
         finishButtons: [
-            { ident: "addButton", text: "Add", onclick: () => { console.log('TODO: add mode.'); } },
+            { ident: "addButton", text: "Add Mode", onclick: () => { console.log('TODO: add mode.'); } },
+            { ident: 'uploadButton', text: 'Upload File', onclick: uploadFile },
             {
                 ident: "downloadButton", text: "Download", onclick: () => {
                     import(/* webpackChunkName: 'dataloading-index-2' */ './loading')
@@ -23,12 +45,6 @@ async function cb(evt) {
     while (root_elm.hasChildNodes()) {
         root_elm.removeChild(root_elm.firstChild);
     }
-    root_elm.appendChild(mode_view.elm);
-}
-
-document.addEventListener("DOMContentLoaded", mymain);
-
-function mymain() {
-    document.getElementById("tomlupload").onchange = cb;
+    root_elm.appendChild(rootView.elm);
 }
 
